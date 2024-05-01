7ff96ebdaf2dfa1c0653a01a9515a5f77878d2a1
Buddhika Prasanna <buddhikaprasanna@Sihans-MacBook-Pro.local>
2024-05-01 14:51:07 +0530

    Implement dashboard activity

--- app/src/main/java/com/example/btechscapepos/Dashboard.kt
+++ app/src/main/java/com/example/btechscapepos/Dashboard.kt
@@ -1,28 +1,20 @@
 package com.example.btechscapepos
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_dashboard)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnLogin = findViewById<Button>(R.id.btnLogin);
-        btnLogin.setOnClickListener {
-            val intent = Intent(this, Dashboard::class.java);
-            startActivity(intent);
         }
     }
 }
